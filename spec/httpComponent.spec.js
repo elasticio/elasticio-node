3534664c285f0653ff69f9eb329b315e650f96ed
Igor Drobiazko <igor@elastic.io>
2014-10-10 09:10:09 +0200

    Minor improvements

--- spec/httpComponent.spec.js
+++ spec/httpComponent.spec.js
@@ -1,11 +1,10 @@
 describe('Http Component', function () {
 
-    var Q = require('q');
     var nock = require('nock');
     var HttpComponent = require('../lib/httpComponent.js').HttpComponent;
     var messages = require('../lib/messages.js');
 
-    it('should work', function () {
+    it('GET', function () {
 
         nock('http://foobarbazbarney.com')
             .get('/api')
@@ -22,8 +21,8 @@ describe('Http Component', function () {
             json: true
         };
 
-        function handleResponse(response) {
-            return Q(messages.newMessageWithBody(response.body));
+        function handleResponse(response, body) {
+            return messages.newMessageWithBody(body);
         }
 
         var component = new HttpComponent(emitter)
@@ -31,7 +30,99 @@ describe('Http Component', function () {
 
         runAndExpect(
             function () {
-                component.exec('get', options);
+                component.get(options);
+            },
+            function () {
+                return emitter.emit.callCount === 2;
+            },
+            function () {
+                var emitCalls = emitter.emit.calls;
+
+                var emitDataArgs = emitCalls[0].args;
+
+                expect(emitDataArgs[0]).toEqual('data');
+                expect(emitDataArgs[1].body).toEqual({
+                    foo: 'bar',
+                    baz: 'barney'
+                });
+
+                expect(emitCalls[1].args).toEqual(['end']);
+            });
+    });
+
+    it('POST', function () {
+
+        nock('http://foobarbazbarney.com')
+            .post('/api')
+            .reply(200, JSON.stringify({
+                foo: 'bar',
+                baz: 'barney'
+            }));
+
+
+        var emitter = jasmine.createSpyObj('emitter', ['emit']);
+
+        var options = {
+            url: 'http://foobarbazbarney.com/api',
+            json: true
+        };
+
+        function handleResponse(response, body) {
+            return messages.newMessageWithBody(body);
+        }
+
+        var component = new HttpComponent(emitter)
+            .success(handleResponse);
+
+        runAndExpect(
+            function () {
+                component.post(options);
+            },
+            function () {
+                return emitter.emit.callCount === 2;
+            },
+            function () {
+                var emitCalls = emitter.emit.calls;
+
+                var emitDataArgs = emitCalls[0].args;
+
+                expect(emitDataArgs[0]).toEqual('data');
+                expect(emitDataArgs[1].body).toEqual({
+                    foo: 'bar',
+                    baz: 'barney'
+                });
+
+                expect(emitCalls[1].args).toEqual(['end']);
+            });
+    });
+
+    it('PUT', function () {
+
+        nock('http://foobarbazbarney.com')
+            .put('/api')
+            .reply(200, JSON.stringify({
+                foo: 'bar',
+                baz: 'barney'
+            }));
+
+
+        var emitter = jasmine.createSpyObj('emitter', ['emit']);
+
+        var options = {
+            url: 'http://foobarbazbarney.com/api',
+            json: true
+        };
+
+        function handleResponse(response, body) {
+            return messages.newMessageWithBody(body);
+        }
+
+        var component = new HttpComponent(emitter)
+            .success(handleResponse);
+
+        runAndExpect(
+            function () {
+                component.put(options);
             },
             function () {
                 return emitter.emit.callCount === 2;
