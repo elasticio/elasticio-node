0a50dc9c39a7a04446d2f6833f4835e354b12252
Igor Drobiazko <igor@elastic.io>
2014-10-21 17:58:30 +0200

    Minor improvement + more tests

--- spec/httpComponent.spec.js
+++ spec/httpComponent.spec.js
@@ -163,6 +163,57 @@ describe('Http Component', function () {
             });
     });
 
+    it('Override success handler', function () {
+
+        nock('http://foobarbazbarney.com')
+            .get('/api')
+            .reply(200, JSON.stringify({
+                foo: 'bar',
+                baz: 'barney'
+            }));
+
+
+        var emitter = jasmine.createSpyObj('emitter', ['emit']);
+
+        var options = {
+            url: 'http://foobarbazbarney.com/api',
+            json: true
+        };
+
+        function onSuccess(response, body) {
+            var msg = messages.newMessageWithBody({
+                wrappedBody : body
+            });
+
+            emitter.emit('data', msg);
+        }
+
+        var component = new HttpComponent(emitter).success(onSuccess);
+
+        runAndExpect(
+            function () {
+                component.get(options);
+            },
+            function () {
+                return emitter.emit.callCount === 2;
+            },
+            function () {
+                var emitCalls = emitter.emit.calls;
+
+                var emitDataArgs = emitCalls[0].args;
+
+                expect(emitDataArgs[0]).toEqual('data');
+                expect(emitDataArgs[1].body).toEqual({
+                    wrappedBody : {
+                        foo: 'bar',
+                        baz: 'barney'
+                    }
+                });
+
+                expect(emitCalls[1].args).toEqual(['end']);
+            });
+    });
+
     function runAndExpect(runner, waiter, expector) {
 
         var next = jasmine.createSpy('next');
