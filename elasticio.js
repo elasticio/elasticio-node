63131a2ed2abc0d5cfb160fa03bc6e8d93f8c573
Pavel Nedelko <pnedelko@gmail.com>
2015-09-22 18:44:40 +0300

    changed link

--- elasticio.js
+++ elasticio.js
@@ -2,5 +2,5 @@ exports.HttpComponent = require("./lib/httpComponent").HttpComponent;
 exports.messages = require("./lib/messages");
 exports.crypt = require("./lib/crypt");
 exports.formats = require("./lib/formats");
-exports.HeartBeatStream = require("./lib/heartbeat-stream");
+exports.HeartBeatStream = require("./lib/heartbeat-stream").HeartBeatStream;
 exports.ObjectUtilities = require("./lib/ObjectUtilities");
